d04626c8151b57cf92547004a8af770478724e78
MjKey <mj.dot.key@gmail.com>
2025-11-28 20:37:15 +0300

    Template Fabric - Start Dev

---
+++ src/client/java/ru/mjkey/ItsMyStoryClient.java
@@ -0,0 +1,10 @@
+package ru.mjkey;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItsMyStoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
